e8063a91ab57c8702b54f62114d28d6d80daf8d6
crcrworks <42961885+crcrworks@users.noreply.github.com>
2024-06-06 18:46:36 +0900

    feat: Itemのinterfaceを追加

---
+++ Assets/Overworld/Scripts/Common/Interface/IItem.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class IItem
+{
+    private interface ItemCommon
+    {
+        string name { get; }
+        string description { get; }
+        int price { get; }
+
+        void Built();
+        void Broken();
+        void Enable();
+        void Disable();
+    }
+
+    private interface ItemClickable { }
+
+    private interface ItemBackpack
+    {
+        Vector2 backpackGridSize { get; }
+        Vector2 backpackGridPosition { get; }
+    }
+
+    public abstract class IItemBlock : ItemCommon, ItemBackpack
+    {
+        public abstract string name { get; }
+        public abstract string description { get; }
+        public abstract int price { get; }
+
+        public Vector2 backpackGridSize { get; }
+        public Vector2 backpackGridPosition { get; }
+
+        public void Built()
+        {
+            Enable();
+        }
+
+        public virtual void Broken()
+        {
+            Disable();
+        }
+
+        public virtual void Enable()
+        {
+            Debug.Log($"{name} is enabled.");
+        }
+
+        public virtual void Disable()
+        {
+            Debug.Log($"{name} is disabled.");
+        }
+    }
+}
