6cc3bb16b066cdb9691982d190d5c13fbf33fc93
crcrworks <42961885+crcrworks@users.noreply.github.com>
2024-07-03 19:00:56 +0900

    chore: itemをprefabから生成するように変更
    このコミットでは、すべてのアイテムのメソッドとプロパティを標準化する新しいインターフェイス IItem
    が導入されています。ItemGeneration クラスは削除され、その機能は IItem インターフェイスと ItemPlaceable
    クラスに移動されました。ItemPointer クラスは、新しい IItem
    インターフェイスを使用するように更新されました。アイテムが現在保持されているかどうかを追跡するために、新しいプロパティ isHolding が
    IItem インターフェイスに追加されました。GameController クラスは、新しい名前空間 Overworld.Model
    を使用するように更新されました。このコミットには、これらの変更を反映するための Unity プロジェクト設定とプレハブの更新も含まれています。

--- Assets/Overworld/Scripts/Model/Item/ItemPlaceable.cs
+++ Assets/Overworld/Scripts/Model/Item/ItemPlaceable.cs
@@ -1,9 +1,100 @@
+using UnityEngine;
+
 namespace Overworld.Model
 {
-    public abstract class ItemPlaceable : ItemGeneration
+    public abstract class ItemPlaceable : IItem
     {
-        public abstract string itemName { get; }
-        public abstract string description { get; }
-        public abstract int price { get; }
+        public bool canBuild { get; private set; } = true;
+
+        private Material TrunslucentShader;
+        private Material HighlightRedShader;
+        private SpriteRenderer spriteRenderer;
+
+        public void Awake()
+        {
+            TrunslucentShader = new Material(Shader.Find("unlit/Translucent"));
+            HighlightRedShader = new Material(Shader.Find("Unlit/HighlightRed"));
+            TryGetComponent<SpriteRenderer>(out spriteRenderer);
+            itemLocationStatus = ItemLocationStatus.World;
+        }
+
+        protected virtual void Update() { }
+
+        protected virtual void FixedUpdate() { }
+
+        public override GameObject OnClick(GameObject itemPrefab)
+        {
+            if (!itemPrefab || !canBuild)
+            {
+                return this.gameObject;
+            }
+
+            switch (itemLocationStatus)
+            {
+                case ItemLocationStatus.World:
+                    return TogglePlaceInWorld(itemPrefab);
+                case ItemLocationStatus.Bag:
+                    return TogglePlaceInBag(itemPrefab);
+                default:
+                    return this.gameObject;
+            }
+        }
+
+        GameObject TogglePlaceInBag(GameObject itemPrefab)
+        {
+            return this.gameObject;
+        }
+
+        GameObject TogglePlaceInWorld(GameObject itemPrefab)
+        {
+            isHolding = !isHolding;
+            var newObject = Instantiate(itemPrefab);
+            newObject.name = this.gameObject.name;
+            newObject.transform.position = this.gameObject.transform.position;
+            newObject.transform.rotation = this.gameObject.transform.rotation;
+
+            {
+                newObject.TryGetComponent<IItem>(out var item);
+                item.isHolding = isHolding;
+            }
+            {
+                newObject.TryGetComponent<Collider2D>(out var collider);
+                collider.isTrigger = isHolding;
+            }
+            {
+                newObject.TryGetComponent<SpriteRenderer>(out var renderer);
+                renderer.material = isHolding
+                    ? TrunslucentShader
+                    : new Material(Shader.Find("Sprites/Default"));
+            }
+
+            Destroy(this.gameObject);
+            return newObject;
+        }
+
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!this.isHolding || !canBuild)
+            {
+                return;
+            }
+            spriteRenderer.material = HighlightRedShader;
+            canBuild = false;
+        }
+
+        public void OnTriggerStay2D(Collider2D other)
+        {
+            OnTriggerEnter2D(other);
+        }
+
+        public void OnTriggerExit2D(Collider2D other)
+        {
+            if (!this.isHolding || canBuild)
+            {
+                return;
+            }
+            spriteRenderer.material = TrunslucentShader;
+            canBuild = true;
+        }
     }
 }
