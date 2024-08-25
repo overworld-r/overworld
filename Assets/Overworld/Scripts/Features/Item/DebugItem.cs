0de41f9ad5e69931101064061b26ea3c1b9fc79d
crcrworks <42961885+crcrworks@users.noreply.github.com>
2024-08-26 00:15:37 +0900

    refacotr: ドッペルゲンガーを退治しました

--- Assets/Overworld/Scripts/Features/Item/DebugItem.cs
+++ Assets/Overworld/Scripts/Features/Item/DebugItem.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 namespace Overworld.Features.Item
 {
-    public class DebugItem : MonoBehaviour, Model.IItemMetadata
+    public class DebugItem : MonoBehaviour, Models.IItemMetadata
     {
         public bool CanBuild { get; private set; } = true;
 
-        string Model.IItemMetadata.itemName { get; set; } = "DebugItem";
-        string Model.IItemMetadata.description { get; set; } = "this is a debug item.";
-        int Model.IItemMetadata.price { get; set; } = 100;
+        string Models.IItemMetadata.itemName { get; set; } = "DebugItem";
+        string Models.IItemMetadata.description { get; set; } = "this is a debug item.";
+        int Models.IItemMetadata.price { get; set; } = 100;
 
         protected void Update() { }
 
--- Assets/Overworld/Scripts/Features/Item/Model/IItemMetadata.cs
+++
@@ -1,9 +0,0 @@
-namespace Overworld.Features.Item.Model
-{
-    public interface IItemMetadata
-    {
-        string itemName { get; internal set; }
-        string description { get; internal set; }
-        int price { get; internal set; }
-    }
-}
