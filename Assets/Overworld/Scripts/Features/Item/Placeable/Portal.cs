a38e7887b42e80e24c173ac8317371759260c449
mutyuki <belandinging01@gmail.com>
2024-08-29 01:55:25 +0900

    ワープ治った！治ったよ！

--- Assets/Overworld/Scripts/Features/Item/Placeable/Portal.cs
+++ Assets/Overworld/Scripts/Features/Item/Placeable/Portal.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using Overworld.Features.CustomCollision;
+using Overworld.Types;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class PortalGate : MonoBehaviour
@@ -15,17 +17,26 @@ public class PortalGate : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (!portalParent.CanWarp())
-            return;
+        Debug.Log("OnTriggerEnter2D called with " + other.name);
 
-        if (other.CompareTag("Player") || other.CompareTag("Item"))
+
+        other.gameObject.OptGetComponent<CustomCollision>(none: () =>
         {
-            WarpPlayer(other.transform, targetPortal.transform);
-        }
+            if (other.CompareTag("Player") || other.CompareTag("Item"))
+            {
+                if (!portalParent.CanWarp())
+                {
+                    Debug.Log("cooltime" + other.name);
+                    return;
+                }
+                WarpPlayer(other.transform, targetPortal.transform);
+            }
+        });
     }
 
     private void WarpPlayer(Transform player, Transform targetWarp)
     {
+        Debug.Log("俺とんだよ" + player.gameObject.name);
         portalParent.StartTimer();
         player.position = targetWarp.position;
     }
